416433347f9f215e3e0a69174637a0310b473bc5
Chris Apolzon <apolzon@gmail.com>
2010-08-26 18:36:21 -0700

    file setup

---
+++ zenleg.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'rest-client'
+class Zenleg
+	# RestClient usage:
+	# RestClient.get 'url'
+	# RestClient.get 'url', {:params => {} }
+	# RestClient.post 'url'
+	# RestClient.delete 'url'
+	
+
+end
