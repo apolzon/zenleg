56fc6b4143477c136bf2f6d2f2b9c6a15ff84cbc
Chris Apolzon <apolzon@gmail.com>
2010-08-27 14:19:43 -0700

    abstracting methods

--- zenleg.rb
+++ zenleg.rb
@@ -5,44 +5,27 @@ require 'ostruct'
 class Zenleg
 	RestClient.log = $stdout
 	@@resource = RestClient::Resource.new("http://applesonthetree.zendesk.com", :user => "apolzon@gmail.com", :password => "test123")
+
 	def initialize(*args)
-		defaults = {
-			:email => "requester1@applesonthetree.com",
+		params = merge_defaults({
+			:email => "requester999@applesonthetree.com",
 			:name => "Request User1",
 			:roles => 0,
 			:restriction_id => 4
-		}
-		params = defaults
-		params = defaults.merge(args.first) unless args.empty?
-		@user = OpenStruct.new
-		@user.email = params[:email]
-		@user.name = params[:name]
-		xml = ""
-		builder = Builder::XmlMarkup.new(:target => xml)
-		builder.instruct!
-		builder.user do |u|
-			u.email params[:email]
-			u.name params[:name]
-			u.roles params[:roles]
-			u.password "testuser"
-			u.tag! "restriction-id", params[:restriction_id]
-			u.tag! "is-verified", "true"
-		end
-		response = @@resource["/users.xml"].post xml, :content_type => "xml"
-		if response.code == 507
-			return "Account cannot create more users"
-		end
-		response
+		}, args)
+		create_user(params)
 	end
 
 	def create_ticket_as_requester(*args)
-		defaults = {
+		params = merge_defaults({
 			:description => "requester default description",
 			:priority_id => 1
-		}
-		params = defaults
-		params = defaults.merge(args.first) unless args.empty?
-		# POST /tickets.xml
+		}, args)
+		xml = build_new_ticket_xml(params)
+		response = @@resource["/tickets.xml"].post xml, :content_type => "xml"
+	end
+
+	def build_new_ticket_xml(params)
 		xml = ""
 		builder = Builder::XmlMarkup.new(:target => xml)
 		builder.instruct!
@@ -52,20 +35,21 @@ class Zenleg
 			t.tag! "requester-name", @user.name
 			t.tag! "requester-email", @user.email 
 		end
-		response = @@resource["/tickets.xml"].post xml, :content_type => "xml"
+		xml
 	end
 
 	def mark_ticket_resolved(*args)
-		defaults = {
+		params = merge_defaults({
 			:assignee_id => 16117939,
 			:additional_tags => "",
 			:ticket_field_entries => [],
-			#:ticket_field_entries => [{:ticket_field_id => 312002, :value => 1}],
 			:id => 2
-		}
-		params = defaults
-		params = defaults.merge(args.first) unless args.empty?
-		# PUT /tickets/#{id}.xml
+		}, args)
+		xml = build_resolve_ticket_xml(params)
+		response = @@resource["/tickets/#{params[:id]}.xml"].put xml, :content_type => "xml"
+	end
+
+	def build_resolve_ticket_xml(params)
 		xml = ""
 		builder = Builder::XmlMarkup.new(:target => xml)
 		builder.instruct!
@@ -84,6 +68,38 @@ class Zenleg
 				end
 			end
 		end
-		response = @@resource["/tickets/#{params[:id]}.xml"].put xml, :content_type => "xml"
+		xml
+	end
+
+	def merge_defaults(defaults, args)
+		params = defaults
+		params = defaults.merge(args.first) unless args.empty?
+		params
+	end
+
+	def create_user(params)
+		@user = OpenStruct.new
+		@user.email = params[:email]
+		@user.name = params[:name]
+		xml = build_user_xml(params)
+		response = @@resource["/users.xml"].post xml, :content_type => "xml"
+		if response.code == 507
+			return "Account cannot create more users"
+		end
+	end
+
+	def build_user_xml(params)
+		xml = ""
+		builder = Builder::XmlMarkup.new(:target => xml)
+		builder.instruct!
+		builder.user do |u|
+			u.email params[:email]
+			u.name params[:name]
+			u.roles params[:roles]
+			u.password "testuser"
+			u.tag! "restriction-id", params[:restriction_id]
+			u.tag! "is-verified", "true"
+		end
+		xml
 	end
 end
